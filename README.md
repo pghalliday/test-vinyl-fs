cc4c3bd3b5aeba9757f0a2a4b06db481a5cc58a6
Peter Halliday <pghalliday@gmail.com>
2014-10-04 18:24:41 +0200

    demo error on symlinked directories

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 test-vinyl-fs
 =============
+
+Test project to demo issue with `vinyl-fs`, `gulp.src()` and symlinked directories
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+  return gulp.src('src/**')
+  .pipe(gulp.dest('dest'));
+});
